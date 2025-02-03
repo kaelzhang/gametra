0db677e7c0de19cce73e92a9e152912609b92a6b
Kael Zhang <i+github@kael.me>
2025-02-03 16:42:18 +0800

    test/ThrottledPerformer: adds more test cases

--- test/throttled-performer.test.js
+++ test/throttled-performer.test.js
@@ -5,24 +5,30 @@ const {setTimeout} = require('node:timers/promises')
 
 const {
   Action,
-  ThrottledPerformer
+  ThrottledPerformer: OriginalThrottledPerformer
 } = require('../src/driver/action')
 
 
-// test('normal action performing', async t => {
-//   class TestAction extends Action {
-//     static Performer = ThrottledPerformer
+class ThrottledPerformer extends OriginalThrottledPerformer {
+  static DEFAULT_OPTIONS = {
+    throttle: 200
+  }
+}
 
-//     async _perform () {
-//       return 1
-//     }
-//   }
+test('normal action performing', async t => {
+  class TestAction extends Action {
+    static Performer = ThrottledPerformer
 
-//   const action = new TestAction()
-//   const result = await action.perform()
+    async _perform () {
+      return 1
+    }
+  }
 
-//   t.is(result, 1)
-// })
+  const action = new TestAction()
+  const result = await action.perform()
+
+  t.is(result, 1)
+})
 
 
 test('throttled action performing', async t => {
@@ -48,10 +54,89 @@ test('throttled action performing', async t => {
   let passed = true
 
   performTimes.reduce((prev, current) => {
-    if (current - prev < 100) {
+    if (current - prev < 200) {
       passed = false
     }
   })
 
   t.true(passed)
+})
+
+
+test('throttled action cancel', async t => {
+  const performTimes = []
+
+  class TestAction extends Action {
+    static Performer = ThrottledPerformer
+
+    async _perform () {
+      performTimes.push(Date.now())
+      return 1
+    }
+  }
+
+  const action = new TestAction()
+  action.perform()
+  action.cancel()
+
+  t.is(performTimes.length, 0)
+})
+
+
+test('throttled action pause', async t => {
+  const start = Date.now()
+
+  class TestAction extends Action {
+    static Performer = ThrottledPerformer
+
+    async _perform () {
+      return Date.now() - start
+    }
+  }
+
+  const action = new TestAction()
+
+  let resolved
+
+  action.perform().then(result => {
+    resolved = result
+  })
+
+  // Call pause before resume, and it should be ok
+  action.resume()
+
+  action.pause()
+  await setTimeout(100)
+  action.resume()
+
+  await setTimeout(1)
+
+  t.true(resolved >= 100)
+})
+
+
+test('action performs after throttle time', async t => {
+  let last = Date.now()
+
+  class TestAction extends Action {
+    static Performer = ThrottledPerformer
+
+    async _perform () {
+      const now = Date.now()
+      const cost = now - last
+      last = now
+      return cost
+    }
+  }
+
+  const action = new TestAction()
+  await action.perform()
+
+  await setTimeout(201)
+
+  last = Date.now()
+
+  const cost = await action.perform()
+
+  t.true(cost < 202)
 })
